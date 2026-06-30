c6327f363d7e26fba690a3757055a6585fc63c49
Nandini Shetty <nandiniby46@gmil.com>
2026-06-30 13:04:00 +0530

    Added registration view

--- tasks/views.py
+++ tasks/views.py
@@ -63,4 +63,18 @@ def edit_task(request, task_id):
             return redirect('task_list')
     else:
         form = TaskForm(instance=task)
-    return render(request, 'tasks/edit_task.html', {'form': form})
+    return render(request, 'tasks/edit_task.html', {'form': form}) 
+
+
+    
+    # Registration View
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
